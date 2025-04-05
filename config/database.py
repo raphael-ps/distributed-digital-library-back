0017debd028081a4e1c2a0056570e593ab79894f
Raphael Pinheiro <70273577+raphael-ps@users.noreply.github.com>
2025-04-05 17:31:24 -0300

    adding readme, requirements and dotenv

--- config/database.py
+++ config/database.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
+import dotenv, os
 
-client = MongoClient(!!! STRING AQUI !!!)
+dotenv.load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URI"))
 
 db = client.library_db
 
